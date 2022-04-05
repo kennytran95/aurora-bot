8c656eb9fd04db97af5d5829b0c76496d6584c45
Kenny <Kennyytran95@gmail.com>
2022-04-05 10:58:52 -0700

    add postinstall

--- bot.js
+++ bot.js
@@ -1,9 +1,3 @@
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
-
 require('dotenv').config()
 
 const Discord = require('discord.js');
